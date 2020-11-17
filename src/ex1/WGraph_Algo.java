9277159a19fbbbb019980a065aba889bc662fa20
yuval1929 <yuval1929@gmail.com>
2020-11-17 14:59:32 +0200

    Added comments to all the functions

--- src/ex1/WGraph_Algo.java
+++ src/ex1/WGraph_Algo.java
@@ -1,34 +1,61 @@
 package ex1;
 import java.io.*;
-
-
 import java.util.*;
+
+    /** This class implements the weighted_graph_algorithms interface which allows preforming complex algorithms on a
+     * weighted, undirected graph, with the following class variables:
+     * weighted_graph _wg - the graph to preform the algorithms on.
+     *  **/
 
 public class WGraph_Algo implements weighted_graph_algorithms, Serializable {
     weighted_graph _wg;
 
+    /** Constructor **/
     public WGraph_Algo(){
         _wg = new WGraph_DS();
     }
 
-
+    /** Initialize the graph to work on the provided weights graph parameter
+     * @param g - the weighted graph to initialize.
+     * @return
+     * **/
     @Override
     public void init(weighted_graph g) {
         _wg = g;
 
     }
 
+    /** Returns the weighted graph in this weighted graph algorithm's as _wg.
+     * @prarm
+     * @return _wg
+     * **/
     @Override
     public weighted_graph getGraph() {
         return _wg;
     }
 
+    /** Returns a deep copy of this graph by sending him to a copy constructor of the WGraph_DS class
+     * @param
+     * @return a deep copy of _wg
+     * **/
     @Override
     public weighted_graph copy() {
         weighted_graph wg_copy = new WGraph_DS(_wg);
         return wg_copy;
     }
 
+    /** This function checks if the current weighted_graph_algo is a connected graph, meaning if their is a path between each 2 nodes in
+     * the graph.
+     * The base assumption behind this function is this: if a graph is a connected graph, from a specific node (each node), there
+     * should be a path to each other node.
+     * So, the functions takes a random node, and adds to a HashMap all the nodes which are connected to him in some path,
+     * by preforming the BFS algorithm.
+     * Then, after adding all the connected nodes to the HashMap, if the number of nodes in the HashMap is equal
+     * to the number of the entire nodes in the graph, hence all the nodes are connected to the random node we chose,
+     * and therefore there is a path between each 2 nodes in the graph, and the graph is connected.
+     * @param
+     * @return True - if the graph is a connected graph, False - if it's not.
+     * **/
     @Override
     public boolean isConnected() {
         Collection<node_info> nodes = _wg.getV();
@@ -56,6 +83,7 @@ public class WGraph_Algo implements weighted_graph_algorithms, Serializable {
         return false;
     }
 
+    /** **/
     @Override
     public double shortestPathDist(int src, int dest) {
         List<node_info> path = shortestPath(src,dest);
@@ -65,6 +93,35 @@ public class WGraph_Algo implements weighted_graph_algorithms, Serializable {
         return -1;
     }
 
+    /** This function returns the shortest path between 2 nodes in a undirected, weighted graph by preforming the Dijkstra() algorithm
+     * function and the reconstructPath() function.
+     * Dijkstra:
+     * Initialization:
+     *      - a priority queue pq, which is prioritized by the tag of each node which will be used to store the shortest distance
+     *      of this current node from the src node.
+     *      - a Double var, dist to Infinity, which will store the current shortest distance from src to dest.
+     *      - Visited Hashset, which will store all the already visited nodes
+     *      - prev Hashmap the map the parent of each node which is the closest (by path weight) to the src node.
+     * The steps:
+     * - Set src tag (distance from src) to 0, and add him to pq.
+     * - Start going over the pq until empty, extract the head (marked as curr) of the pq and go over is neighbors (if his not already visited).
+     * - For each neighbor, define the current distance from src (in the path that goes through curr), and check if the current distance
+     * is shortest then the current shortest distance from src to path, if not - there is no point to continue with this neighbor.
+     * - If so, check if the current neighbor is the dest node, if so, replace distance var with the ni_dist_from_src var.
+     * - Check if the current neighbor as a parent node marked in the prev Hashmap,  if not - set curr as his parent and change the
+     * neighbor's tag to ni_dist_from_src, if is does have a parent node, change the parent node and the tag of the neighbor only if
+     * ni_dist_from_src is smaller than the current tag of this neighbor.
+     * - Add the neighbor to the pq.
+     * Go thorough this process for each edge in the graph until the pq is empty and return the prev hashmap.
+     *
+     * reconstructPath:
+     * This function takes the prev HashMap from the solve function and the src and dest nodes,
+     * and is trying to construct a path between dest to src (the reversed way) by adding to a list the prev of dest,
+     * and then the prev of the prev of dest, and so on, until it reached the src node, if it does - its the shortest path
+     * between src and dest, if it can't reach the src node - there is no path between src and dest.
+     * The function ends by reversing the path (constructed as an ArrayList) to make if from src to dest and not
+     * dest to src.
+     * **/
     @Override
     public List<node_info> shortestPath(int src, int dest) {
         node_info src_node = _wg.getNode(src);
@@ -72,16 +129,13 @@ public class WGraph_Algo implements weighted_graph_algorithms, Serializable {
         HashMap<Integer, node_info> prev = Dijkstra(src_node, dest_node);
         List<node_info> path = reconstructPath(prev, src_node, dest_node);
         return path;
-
     }
 
-
     public HashMap<Integer, node_info> Dijkstra (node_info src, node_info dest){
-        PriorityQueue<node_info> pq = new PriorityQueue<node_info>((node1, node2) -> Double.compare(node1.getTag(), node2.getTag()));
+        PriorityQueue<node_info> pq = new PriorityQueue<node_info>(Comparator.comparingDouble(node_info::getTag));
         double dist = Double.POSITIVE_INFINITY;
         HashSet<Integer> visited = new HashSet<Integer>();
         HashMap<Integer, node_info> prev = new HashMap<Integer, node_info>();
-//        HashMap<Integer, HashMap<Integer, node_info>> prev = new  HashMap<Integer, HashMap<Integer, node_info>>();
         src.setTag(0);
         pq.add(src);
         while (!pq.isEmpty()){
@@ -133,6 +187,10 @@ public class WGraph_Algo implements weighted_graph_algorithms, Serializable {
 
     }
 
+    /** This function saves _wg to a file as a string of bytes using the Serializable interface in a way that will allow us to load it again.
+     * @param file - the name of the file to save _wg to.
+     * @return True if successfully save, False otherwise.
+     * **/
     @Override
     public boolean save(String file) {
         boolean flag = true;
@@ -152,6 +210,11 @@ public class WGraph_Algo implements weighted_graph_algorithms, Serializable {
 
     }
 
+    /**This function load a weighted graph from a file and is intializing him to be the weighted graph of this
+     * weighted graph algorithm.
+     * @param file - the file to load from the weighted graph
+     * @return True if successfully load and intiliaze, False otherwise.
+     * **/
     @Override
     public boolean load(String file) {
         boolean flag = true;
--- src/ex1/WGraph_DS.java
+++ src/ex1/WGraph_DS.java
@@ -5,12 +5,25 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 
+    /**
+     * @author Yuval Moshe
+     *
+     * This class implements the weighted_graph interface which is a weighted undirected graph with the following class variables:
+     * HashMap<Integer, node_info> _vertices  - Reperesents the vertices in the graph as a hashmap which maps each node to his unique key.
+     * HashMap<Integer, HashMap<Integer, Double>> _weights  - represents the edges and their wieght in the graph, by mapping the key of each
+     * node in the graph to an hashmap which maps all the neighbors (by their key) of this specific node, to the weight of the edge between
+     * the node and his neighbor.
+     * int _numOfEdges = number of edges in the grpah.
+     * int _mc = number of actions preformed on the the graph.
+     * **/
+
 public class WGraph_DS implements weighted_graph, Serializable {
     HashMap<Integer, node_info> _vertices = new HashMap<Integer, node_info>();
     HashMap<Integer, HashMap<Integer, Double>> _weights = new HashMap<Integer, HashMap<Integer, Double>>();
     int _numOfEdges;
     int _mc;
 
+    /** Constructor **/
     public WGraph_DS(){
         _vertices = new HashMap<Integer, node_info>();
         _weights = new HashMap<Integer, HashMap<Integer, Double>>();
@@ -18,7 +31,7 @@ public class WGraph_DS implements weighted_graph, Serializable {
         _mc = 0;
 
     }
-
+    /** Copy Constructor **/
     public WGraph_DS (weighted_graph other){
         Collection<node_info> other_v = other.getV();
         for(node_info node : other_v){
@@ -37,13 +50,22 @@ public class WGraph_DS implements weighted_graph, Serializable {
         _mc = other.getMC();
     }
 
-
+    /** Retrieves the specific node from the graph, by returning the mapped node_info the provided key.
+     * @param key  - the key of the node to retrieve.
+     * @return  node_info - the node to retrieve.
+     * **/
     @Override
     public node_info getNode(int key) {
         return _vertices.get(key);
     }
 
+    /** Checks if theres an edge between 2 nodes in the grpah, by checking if their each other's neighbors Hashmap.
+     * @param node1 - node1's key.
+     * @param node2 - node2'2 key.
+     * @return  True if there is an edge between node1 and node2, False if there isn't.
+     * **/
     @Override
+    //לבדוק בנוגע לאותו קוד
     public boolean hasEdge(int node1, int node2) {
         if(node1!=node2){
             if(_vertices.containsKey(node1) && _vertices.containsKey(node2)){
@@ -53,6 +75,11 @@ public class WGraph_DS implements weighted_graph, Serializable {
         return false;
     }
 
+    /**Returns the weight of the edge between 2 nodes (if exists) by checking the value of the mapped neighbor key.
+     * @param node1 - node1's key.
+     * @param node2 - node2's key.
+     * @return - weight of the edge between them, if there isn't any return -1;
+     *  **/
     @Override
     public double getEdge(int node1, int node2) {
         if(this.hasEdge(node1, node2)){
@@ -61,6 +88,10 @@ public class WGraph_DS implements weighted_graph, Serializable {
         return -1;
     }
 
+    /** Adds a node to the graph by mapping his key to a new node_info
+     * @param key - the unique key of the node to add/
+     * @return
+     * **/
     @Override
     public void addNode(int key) {
         if(_vertices.get(key)==null) {
@@ -71,8 +102,13 @@ public class WGraph_DS implements weighted_graph, Serializable {
 
     }
 
-    // test what happens if there already a weight betweehn them (should update)
-    // What to do if they are the same
+    /** Connects 2 nodes by an edge with weight w, by adding each ones to the other's neighbors hasmap and maping the neighbor's
+     * key the the provided weight.
+     * @param node1 - node1's key.
+     * @param node2 - node2's key.
+     * @param w - the weights of the edge to add between node1 and node 2.
+     * @return
+     * **/
     @Override
     public void connect(int node1, int node2, double w) {
         if(w>=0) {
@@ -92,24 +128,38 @@ public class WGraph_DS implements weighted_graph, Serializable {
         }
     }
 
+    /** Returns all the vertices in the graph by return the values of the _vertices hashmap (O(1) time complexity)
+     * @param
+     * @return
+     * **/
+
     @Override
     public Collection<node_info> getV() {
         return _vertices.values();
     }
 
+    /** Returns all the vertices in the graph which are connected to the provided node key, be returning all the node's neighbors
+     * from the _weights Hashmap
+     * @param node_id - the node's key
+     * @return an Arraylist of all the conncted vertices to this node, if the nodes not in the graph/ the node is not connected
+     * to any other vertices, returns any empty Arraylist**/
     @Override
     public Collection<node_info> getV(int node_id) {
+        Collection<node_info> neighbors = new ArrayList<node_info>();
         if(_vertices.containsKey(node_id)){
             Collection<Integer> neighbors_keys = _weights.get(node_id).keySet();
-            Collection<node_info> neighbors = new ArrayList<node_info>();
             for(Integer nodeKey : neighbors_keys){
                 neighbors.add(_vertices.get(nodeKey));
             }
-            return neighbors;
         }
-        return new ArrayList<node_info>();
+        return neighbors;
     }
 
+    /** Removes the specific node from the graph (if exsits) by first removing him
+     * from each of his neighbors neighbor's Hashmap and them removing him from the _vertices Hashmap.
+     * @param key
+     * @return
+     */
     @Override
     public node_info removeNode(int key) {
         node_info temp = _vertices.get(key);
@@ -125,6 +175,11 @@ public class WGraph_DS implements weighted_graph, Serializable {
         return temp;
     }
 
+    /** Removes an edge between 2 nodes (if exists) by removing each other from their _weights map.
+     * @param node1 - node1's key.
+     * @param node2 - node2's key.
+     * @return
+     */
     @Override
     public void removeEdge(int node1, int node2) {
         if(hasEdge(node1,node2)){
@@ -136,21 +191,32 @@ public class WGraph_DS implements weighted_graph, Serializable {
 
     }
 
+    /** returns the number of nodes in the graph by returning the num of mapped keys in _vertices Hashmap
+     * @return number of nodes.
+     */
     @Override
     public int nodeSize() {
         return _vertices.size();
     }
 
+        /** returns _numOfEdges variable
+         * @return number of edges.
+         */
     @Override
     public int edgeSize() {
         return _numOfEdges;
     }
 
+    /** returns the number of actions preformed on the graph by returning the _mc var.
+     * @return number of actions
+     */
     @Override
     public int getMC() {
         return _mc;
     }
 
+    /** toString function
+     * @return string representation of the graph**/
     public String toString (){
         String s="";
         for(node_info node : _vertices.values()){
@@ -161,58 +227,119 @@ public class WGraph_DS implements weighted_graph, Serializable {
         return s;
     }
 
+    /** Equals function for graph comparison, checks if each node & edge in this graph are also in the compared graph
+     * and vice versa
+     * @param WG_2 - the graph to compare to
+     * @return True if the graphs are equals, False if not
+     * **/
+    public boolean equals (weighted_graph WG_2){
+        boolean flag = true;
+        Collection<node_info> WG1_V = getV();
+        Collection<node_info> WG2_V = WG_2.getV();
+        for(node_info node1 : WG1_V){
+            flag &= (WG_2.getNode(node1.getKey())!=null);
+        }
+        for(node_info node2 : WG1_V){
+            flag &= (getNode(node2.getKey())!=null);
+        }
+        for(node_info node : WG_2.getV()){
+            for(node_info ni : WG_2.getV()){
+                if(WG_2.hasEdge(node.getKey(), ni.getKey())){
+                    flag &= hasEdge(node.getKey(), ni.getKey());
+                }
+            }
+        }
+        for(node_info node : getV()){
+            for(node_info ni : getV()){
+                if(hasEdge(node.getKey(), ni.getKey())){
+                    flag &= WG_2.hasEdge(node.getKey(), ni.getKey());
+                }
+            }
+        }
+        return flag;
+    }
+
+    /** This class implements the node_info interface which represents node in an unweighted undirected graph with the following class variables:
+     * String _info - the information stored in the node.
+     * double _tag - a temporarily tag assigned to this node
+     * int _key - the unique key of the node
+     * **/
+
+
     private class NodeInfo implements node_info, Serializable{
         private String _info;
         private double _tag;
         private int _key;
 
+
+        /** Constructor
+         * @param key - the unique key of this node **/
         public NodeInfo(int key){
             _key = key;
             _info ="";
             _tag=0.0;
         }
+        /** Copy Constructor **/
         public NodeInfo (node_info other){
             _info = other.getInfo();
             _tag = other.getTag();
 
         }
 
+        /** returns the key of this node.
+         * @return _key.
+         * **/
         @Override
         public int getKey() {
             return _key;
         }
 
+        /** return the info stored in the node.
+         * @return _info.
+         * **/
         @Override
         public String getInfo() {
             return _info;
         }
 
+        /** changes the info stored in the node to the provided String
+         * @param s - the new info to set as _info
+         * @return
+         * **/
         @Override
         public void setInfo(String s) {
             _info = s;
         }
 
+        /** returns the tag of this node.
+         * @return _tag.
+         * **/
         @Override
         public double getTag() {
             return _tag;
         }
 
+        /** changes the tag of this node to the provided double
+         * @param t - the new tag to set as _tag
+         * @return
+         * **/
         @Override
         public void setTag(double t) {
             _tag = t;
 
         }
 
-        public int compareTo(node_info other){
-            if(_tag == other.getTag()){
-                return 0;
-            }
-            else if (_tag > other.getKey()){
-                return 1;
-            }
-            else {
-                return -1;
-            }
-        }
+
+//        public int compareTo(node_info other){
+//            if(_tag == other.getTag()){
+//                return 0;
+//            }
+//            else if (_tag > other.getKey()){
+//                return 1;
+//            }
+//            else {
+//                return -1;
+//            }
+//        }
     }
 }
