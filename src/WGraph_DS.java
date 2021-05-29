33f0fbd66d96a168c79d69009dd3abcf4106a88e
yuval1929 <yuval1929@gmail.com>
2021-05-29 14:57:12 +0300

    Rearranged repository structure

--- src/WGraph_DS.java
+++ src/WGraph_DS.java
@@ -1,4 +1,3 @@
-package ex1.src;
 
 import java.io.Serializable;
 import java.util.ArrayList;
--- tests/WGraph_DS_Test.java
+++ tests/WGraph_DS_Test.java
@@ -1,7 +1,3 @@
-package ex1.tests;
-import ex1.src.WGraph_DS;
-import ex1.src.node_info;
-import ex1.src.weighted_graph;
 import org.junit.jupiter.api.Test;
 import java.util.*;
 import static org.junit.jupiter.api.Assertions.*;
